f13a232efdeaba4966dd8022e8d2d6c6de0186c1
kneumann2023 <karolina.chmelikova@gmail.com>
2023-10-03 17:10:46 +0200

    ukol_ZK

--- ukol_ZK.py
+++ ukol_ZK.py
@@ -1,2 +1,8 @@
 jmeno = "karolina"
 print(jmeno + "@czechitas.cz")
+
+jmeno_a_prijmeni = input("Zadejte Vaše jméno a příjmení" )
+print(jmeno_a_prijmeni)
+print(jmeno_a_prijmeni.upper())
+
+# sorry, víc teď nestihnu :)
